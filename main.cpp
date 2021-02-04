c2c36f47c98f2d276eb9cda5eac2f8cba33f22ce
Bendegúz Cseresnyés <cser.bende@gmail.com>
2021-02-04 18:24:03 +0100

    1. feladat megoldása

--- main.cpp
+++ main.cpp
@@ -11,24 +11,52 @@ using namespace std;
     feltehető, hogy legalább 1 eleme van az adatoknak
 */
 
-//#define NORMAL_MODE
+#define NORMAL_MODE
 #ifdef NORMAL_MODE
     int main()
     {
         // file megnyitása, hiba esetén kilépés
+        ifstream f( "input.txt" );
+        if(f.fail()){
+            cout << "Wrong file name!\n";
+            return 1;
+        }
 
-        // adatok beolvasása vector-ba
+        // adatok beolvasása vector-ba 
+        vector<int> x;
+        int e;
+        while(f >> e) {
+            x.push_back(e);
+        }
 
         // leggyakoribb elem megkerestetése
+        if(x.size()==0) cout << "Üres input\n";
+        else cout << "Leggyakoribb elem: " << mostFrequented(x) << endl;
+
+        return 0;
     }
 #else
     #include "test.hpp"
 #endif
 
 int frequency(const vector<int> &x, int e){
-    return 0;
+    int cnt = 0;
+    for(unsigned int i = 0; i < x.size(); i++){
+        if(x[i] == e) cnt++;
+    }
+    return cnt;
 }
 
 int mostFrequented(const vector<int> &x){
-    return 0;
+    unsigned int mfi = 0;
+    int mfc = frequency(x, x[0]);
+
+    for(unsigned int i = 1; i < x.size(); i++){
+        int c = frequency(x, x[i]);
+        if(c > mfc) {
+            mfi = i;
+            mfc = c;
+        }
+    }
+    return x[mfi];
 }
