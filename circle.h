2537e6e5c37a95065cd62ef9fad56f406ae5bc92
Bendegúz Cseresnyés <cser.bende@gmail.com>
2021-02-18 09:39:23 +0100

    Add solution

--- circle.h
+++ circle.h
@@ -5,3 +5,18 @@
   Konstruktor
   contains(Point)
 */
+
+class Circle
+{
+private:
+  Point _c;
+  double _r;
+
+public:
+  Circle() {}
+  Circle(const Point &p, double d) : _c(p), _r(d) {}
+  bool contains(const Point &p) const
+  {
+    return _c.distance(p) <= _r;
+  }
+};
--- point.h
+++ point.h
@@ -1,7 +1,21 @@
 #pragma once
+#include <cmath>
 
 /*
   Koordináták: x, y
   konstruktor
   distance(Point)
 */
+
+class Point
+{
+public:
+  double _x, _y;
+
+public:
+  Point(double a = 0, double b = 0) : _x(a), _y(b) {}
+  double distance(const Point &p) const
+  {
+    return sqrt(pow(_x - p._x, 2) + pow(_y - p._y, 2));
+  }
+};
