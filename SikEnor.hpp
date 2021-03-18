da20bbc696b7eb1e8dc47b6eb3d4895983d1007e
Bendegúz Cseresnyés <cser.bende@gmail.com>
2021-03-18 10:00:47 +0100

    Remove solution parts

--- SikEnor.hpp
+++ SikEnor.hpp
@@ -28,7 +28,6 @@ private:
 
     // Enor mukodes
     Sik _item;
-    bool _end;
 
 public:
     SikEnor(string filename)
@@ -37,41 +36,18 @@ public:
     }
     void first();
     void next();
-    bool end() { return _end; }
+    bool end();
     Sik current() { return _item; }
 };
 
 void SikEnor::read()
 {
-    _x >> _e;
-    _st = _x.fail() ? ABNORM : NORM;
 }
 
 void SikEnor::next()
 {
-    if (_st == ABNORM)
-    {
-        _end = true;
-        return;
-    }
-    int cnt = 1;
-    int sum = _e;
-    int prev = _e;
-    read();
-
-    while (_st != ABNORM && prev - _e < 10 && _e - prev < 10)
-    {
-        cnt++;
-        sum += _e;
-        read();
-    }
-
-    _item.length = cnt;
-    _item.avg = 1.0 * sum / cnt;
 }
 
 void SikEnor::first()
 {
-    read(); // kezdő érték beolvasása e-be
-    next();
 }
