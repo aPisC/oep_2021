664fb2ce4b5fadb721f0c400f3ba4f817557bb3c
Bendegúz Cseresnyés <cser.bende@gmail.com>
2021-03-18 09:57:30 +0100

    Add main logic Remove HorpadEnor

--- main.cpp
+++ main.cpp
@@ -3,14 +3,31 @@
 
 using namespace std;
 
+// Leghosszabb sík keresése, milyen magas ez a sík?
+//  Síknak tekintjük azt az egybefüggő szakaszt,
+//  ahol az egymást követő magasságok eltérése 10nél kisebb
+
 int main()
 {
+    bool l = false;
+    Sik max;
+
     SikEnor t("input.txt");
     t.first();
 
     while (!t.end())
     {
-        cout << t.current().avg << endl;
+        cout << "Vizsgalt sik: " << t.current().length << " " << t.current().avg << endl;
+        if (!l || t.current().length > max.length)
+        {
+            l = true;
+            max = t.current();
+        }
         t.next();
     }
+
+    if (!l)
+        cout << "Nem volt sik" << endl;
+    else
+        cout << "Leghosszabb sik " << max.length << " hosszu es magassaga: " << max.avg << endl;
 }
