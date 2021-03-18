d234505eb6c013b78c7bbff7b642285884c61151
Bendegúz Cseresnyés <cser.bende@gmail.com>
2021-03-18 09:51:18 +0100

    Add 6_3 solution

---
+++ SikEnor.hpp
@@ -0,0 +1,73 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+struct Sik
+{
+    int length;
+    double avg;
+};
+
+class SikEnor
+{
+private:
+    enum Status
+    {
+        NORM,
+        ABNORM
+    };
+    Status _st;
+    int _e;
+    Sik _item;
+    ifstream _x;
+    bool _end;
+    void read();
+
+public:
+    SikEnor(string filename)
+    {
+        _x.open(filename.c_str());
+    }
+    void first();
+    void next();
+    bool end() { return _end; }
+    Sik current() { return _item; }
+};
+
+void SikEnor::read()
+{
+    _x >> _e;
+    _st = _x.fail() ? ABNORM : NORM;
+}
+
+void SikEnor::next()
+{
+    if (_st == ABNORM)
+    {
+        _end = true;
+        return;
+    }
+    int cnt = 1;
+    int sum = _e;
+    int prev = _e;
+    read();
+
+    while (_st != ABNORM && prev - _e < 10 && _e - prev < 10)
+    {
+        cnt++;
+        sum += _e;
+        read();
+    }
+
+    _item.length = cnt;
+    _item.avg = 1.0 * sum / cnt;
+}
+
+void SikEnor::first()
+{
+    read(); // kezdő érték beolvasása e-be
+    next();
+}
--- main.cpp
+++ main.cpp
@@ -1,28 +1,16 @@
 #include <iostream>
-#include "HorpadEnor.hpp"
+#include "SikEnor.hpp"
 
 using namespace std;
 
 int main()
 {
-    HorpadEnor t("input.txt");
-
-    bool l = false;
-    Item max;
-
+    SikEnor t("input.txt");
     t.first();
+
     while (!t.end())
     {
-        if (t.current().isHorpadas() && (!l || max.a < t.current().a))
-        {
-            l = true;
-            max = t.current();
-        }
+        cout << t.current().avg << endl;
         t.next();
     }
-
-    if (!l)
-        cout << "Nem talaltam horpadast" << endl;
-    else
-        cout << "Legmagasabb horpadas: " << max.a << endl;
 }
